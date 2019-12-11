dcd5792d482d9b3ef4135bd4b14ab8b966195d2b
Leo <losoviz@gmail.com>
2019-12-11 10:23:12 +0800

    Renamed getTypeCollectionName to getTypeName

--- src/TypeResolvers/HighlightTypeResolver.php
+++ src/TypeResolvers/HighlightTypeResolver.php
@@ -1,11 +1,10 @@
 <?php
 namespace PoP\Highlights\TypeResolvers;
 
-use PoP\ComponentModel\Utils;
 use PoP\Posts\TypeResolvers\PostTypeResolver;
 
 class HighlightTypeResolver extends PostTypeResolver
 {
-    
+
 }
 
