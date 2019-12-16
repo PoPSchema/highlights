3446361d2cb884d13b8251c9a43790bfb93cc303
Leo <losoviz@gmail.com>
2019-12-16 17:48:54 +0800

    TypeResolver doesn't extend from Post

--- src/TypeResolvers/HighlightTypeResolver.php
+++ src/TypeResolvers/HighlightTypeResolver.php
@@ -1,11 +1,32 @@
 <?php
 namespace PoP\Highlights\TypeResolvers;
 
-use PoP\Posts\TypeResolvers\PostTypeResolver;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 use PoP\Highlights\TypeDataLoaders\HighlightTypeDataLoader;
 
-class HighlightTypeResolver extends PostTypeResolver
+class HighlightTypeResolver extends AbstractTypeResolver
 {
+	public const NAME = 'Highlight';
+
+    public function getTypeName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSchemaTypeDescription(): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return $translationAPI->__('A highlighted piece of text, extracted from a post', 'highlights');
+    }
+
+    public function getId($resultItem)
+    {
+        $cmspostsresolver = \PoP\Posts\ObjectPropertyResolverFactory::getInstance();
+        $post = $resultItem;
+        return $cmspostsresolver->getPostId($post);
+    }
+
     public function getTypeDataLoaderClass(): string
     {
         return HighlightTypeDataLoader::class;
