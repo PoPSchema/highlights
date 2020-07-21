64242738ea3f1c788c074c0d73472a47503a461e
Leonardo Losoviz <leo@getpop.org>
2020-07-21 20:41:00 +0800

    Namespaced constant

--- src/FieldResolvers/CustomPostFieldResolver.php
+++ src/FieldResolvers/CustomPostFieldResolver.php
@@ -87,7 +87,7 @@ class CustomPostFieldResolver extends AbstractDBDataFieldResolver
                     'order' => 'ASC',
                 );
 
-                return $customPostTypeAPI->getCustomPosts($query, ['return-type' => POP_RETURNTYPE_IDS]);
+                return $customPostTypeAPI->getCustomPosts($query, ['return-type' => \POP_RETURNTYPE_IDS]);
 
             case 'hasHighlights':
                 $referencedbyCount = $typeResolver->resolveValue($resultItem, 'highlightsCount', $variables, $expressions, $options);
