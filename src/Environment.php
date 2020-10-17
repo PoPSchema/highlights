c15600b4fccb88900d191d4a7533a67f3d39208a
Leonardo Losoviz <leo@getpop.org>
2020-10-17 22:03:20 +0800

    Use `getenv` instead of `$_ENV` To support SetEnv in .htaccess

--- src/Environment.php
+++ src/Environment.php
@@ -8,6 +8,6 @@ class Environment
 {
     public static function addHighlightTypeToCustomPostUnionTypes(): bool
     {
-        return isset($_ENV['ADD_HIGHLIGHT_TYPE_TO_CUSTOMPOST_UNION_TYPES']) ? strtolower($_ENV['ADD_HIGHLIGHT_TYPE_TO_CUSTOMPOST_UNION_TYPES']) == "true" : false;
+        return getenv('ADD_HIGHLIGHT_TYPE_TO_CUSTOMPOST_UNION_TYPES') !== false ? strtolower(getenv('ADD_HIGHLIGHT_TYPE_TO_CUSTOMPOST_UNION_TYPES')) == "true" : false;
     }
 }
